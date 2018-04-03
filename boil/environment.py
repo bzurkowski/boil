d40c7b30fc8ae1e6a2b94dea5b42731493c930e0
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2018-04-03 23:22:14 +0200

    Wrap Jinja environment into custom  class

--- boil/environment.py
+++ boil/environment.py
@@ -1,11 +1,20 @@
-from jinja2 import Environment, PackageLoader
+import jinja2
 
 from boil import filters
 
 
-def get(plate_module):
-    env = Environment(
-        loader=PackageLoader(plate_module.__name__),
-        keep_trailing_newline=True)
-    env.filters.update(filters.TEMPLATE_FILTERS)
-    return env
+class PlateEnvironment(jinja2.Environment):
+
+    def __init__(self, plate, **kwargs):
+        self.plate = plate
+        super(PlateEnvironment, self).__init__(
+            loader=jinja2.PackageLoader(plate.__name__),
+            keep_trailing_newline=True, **kwargs)
+        self._setup_filters()
+
+    def _setup_filters(self):
+        self.filters.update(filters.TEMPLATE_FILTERS)
+
+
+def get(plate, **kwargs):
+    return PlateEnvironment(plate, **kwargs)
