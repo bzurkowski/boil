e794a3bb6baa73dbfcf7623552158ced9a610737
Bartosz Żurkowski <zurkowski.bartosz@gmail.com>
2018-07-19 22:51:24 +0200

    Add license plate

---
+++ boil/plates/license/__init__.py
@@ -0,0 +1,14 @@
+VARS = [
+    {'name': 'license_type',
+     'required': True,
+     'default': 'mit'},
+    {'name': 'product_name',
+     'required': True,
+     'example': 'Super fancy product'},
+    {'name': 'author',
+     'required': True,
+     'example': 'John Smith or name of a company'},
+    {'name': 'year',
+     'required': True,
+     'example': '2018'}
+]
