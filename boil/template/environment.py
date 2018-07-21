198424600f7401e1ab2e801cafa9ab0cb2a3bc6d
Bartosz Żurkowski <zurkowski.bartosz@gmail.com>
2018-07-21 14:13:36 +0200

    Trim Jinja blocks in templates

--- boil/template/environment.py
+++ boil/template/environment.py
@@ -5,14 +5,14 @@ from boil.common import filters
 
 class Environment(jinja2.Environment):
 
-    def __init__(self, package_name, **kwargs):
-        self._set_default_options(kwargs)
+    def __init__(self, package_name):
         loader = jinja2.PackageLoader(package_name)
-        super(Environment, self).__init__(loader=loader, **kwargs)
+        super(Environment, self).__init__(
+            loader=loader,
+            keep_trailing_newline=True,
+            trim_blocks=True,
+            lstrip_blocks=True)
         self._setup_filters()
-
-    def _set_default_options(self, kwargs):
-        kwargs.setdefault('keep_trailing_newline', True)
 
     def _setup_filters(self):
         self.filters.update(filters.TEMPLATE_FILTERS.copy())
