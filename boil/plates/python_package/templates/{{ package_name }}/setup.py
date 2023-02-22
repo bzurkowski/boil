242ffff80656ebf3abed741bc1734888558b17a9
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2023-02-22 08:03:55 +0100

    Add test tox environment to Python package plate
    Signed-off-by: Bartosz Zurkowski <zurkowski.bartosz@gmail.com>

--- boil/plates/python_package/templates/{{ package_name }}/setup.py
+++ boil/plates/python_package/templates/{{ package_name }}/setup.py
@@ -2,23 +2,24 @@ from setuptools import setup, find_packages
 
 
 def readme():
-    with open('README.rst') as f:
+    with open("README.rst") as f:
         return f.read()
 
 
 def get_requirements():
-    with open('requirements.txt') as requirements:
+    with open("requirements.txt") as requirements:
         return requirements.read().splitlines()
 
 
 setup(
-    name='{{ package_name | underscore }}',
-    version='0.1.0',
-    description='{{ description }}',
+    name="{{ package_name | underscore }}",
+    version="0.1.0",
+    description="{{ description }}",
     long_description=readme(),
-    url='{{ url }}',
-    author='{{ author }}',
-    license='{{ license }}',
+    url="{{ url }}",
+    author="{{ author }}",
+    license="{{ license }}",
     install_requires=get_requirements(),
     packages=find_packages(),
-    zip_safe=False)
+    zip_safe=False
+)
--- boil/plates/python_package/templates/{{ package_name }}/tests/unit/test_example.py
+++ boil/plates/python_package/templates/{{ package_name }}/tests/unit/test_example.py
@@ -2,6 +2,5 @@ import unittest
 
 
 class ExampleTest(unittest.TestCase):
-
     def test_anything(self):
         self.assertEqual(1, 1)
